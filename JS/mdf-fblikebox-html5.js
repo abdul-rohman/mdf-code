acfc3c9be63a31863b970d7f4977e3f0edf1d7e2
boy.cmg.comp@gmail.com <boy.cmg.comp@gmail.com@dc0b6905-7da2-53a3-6372-658460c6fc9e>
2013-05-01 08:04:58 +0000

    delete

--- JS/mdf-fblikebox-html5.js
+++
@@ -1,7 +0,0 @@
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/id_ID/all.js#xfbml=1&appId=134154613417459";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
--- JS/potongsnipetpopularpost.js
+++
@@ -1,9 +0,0 @@
-//<![CDATA[
-// potong snipet popular post
-$('.popular-posts ul li .item-snippet').each(function(){
-  var txt=$(this).text().substr(0,70);
-  var j=txt.lastIndexOf(' ');
-  if(j>42)
-     $(this).text(txt.substr(0,j).replace(/[?,!\.-:;]*$/,'...'));
-});
-//]]>
